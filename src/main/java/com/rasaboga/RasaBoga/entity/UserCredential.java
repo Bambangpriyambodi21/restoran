9d399b9feacad55846a944e5fee95ecdf51e0638
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-05-04 21:38:13 +0700

    [IMPROVE] improve class entity UserCredential

--- src/main/java/com/rasaboga/RasaBoga/entity/UserCredential.java
+++ src/main/java/com/rasaboga/RasaBoga/entity/UserCredential.java
@@ -2,7 +2,11 @@ package com.rasaboga.RasaBoga.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -12,7 +16,7 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "m_user_credential")
-public class UserCredential {
+public class UserCredential implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,4 +26,34 @@ public class UserCredential {
     private String password;
     @ManyToMany
     private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRole().name())).toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
