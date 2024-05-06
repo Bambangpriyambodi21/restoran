73f0ebb2d5b25d589fa13b196ed400feb21f65dd
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-05-06 09:50:06 +0700

    [IMPROVE] improve login class JwtUtils

--- src/main/java/com/rasaboga/RasaBoga/security/JwtUtils.java
+++ src/main/java/com/rasaboga/RasaBoga/security/JwtUtils.java
@@ -1,9 +1,14 @@
 package com.rasaboga.RasaBoga.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.rasaboga.RasaBoga.entity.UserCredential;
+import com.rasaboga.RasaBoga.model.JwtClaims;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +17,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 @Component
+@Slf4j
 public class JwtUtils {
 
     @Value("${app.restoran.jwt-secret}")
@@ -33,6 +39,36 @@ public class JwtUtils {
                     .sign(algorithm);
         }catch (JWTCreationException e){
             throw new RuntimeException(e);
+        }
+    }
+
+    public Boolean verifyJwtToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(secretKey);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getIssuer().equals(appName);
+        }catch (JWTVerificationException e){
+            log.error("invalid verification JWT: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public JwtClaims getUserInfoByToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(secretKey);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+
+            List<String> roles = decodedJWT.getClaim("roles").asList(String.class);
+
+            return JwtClaims.builder()
+                    .userId(decodedJWT.getSubject())
+                    .roles(roles)
+                    .build();
+        }catch (JWTVerificationException e){
+            log.error("invalid verification JWT: {}", e.getMessage());
+            return null;
         }
     }
 }
